197c88da2d5a992430b52f11339e8406ba878274
rendipriyatna <167002063@student.unsil.ac.id>
2022-01-14 21:46:11 +0700

    mengubah site

--- js/custom.js
+++ js/custom.js
@@ -113,7 +113,7 @@
     Countdown Clock
     ................................................. */
 	function makeTimer() {
-		var endTime = new Date("01 May 2022 10:45:00 GMT+01:00");			
+		var endTime = new Date("05 May 2022 10:45:00 GMT+01:00");			
 		endTime = (Date.parse(endTime) / 1000);
 
 		var now = new Date();
